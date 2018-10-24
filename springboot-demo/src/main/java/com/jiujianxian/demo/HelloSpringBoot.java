e80c373287b6a5b80825a24ceadb9dde86639c97
zhangwei24863 <zhangwei24863@hundsun.com>
2018-10-24 09:38:09 +0800

    spring boot  演示案例1

--- springboot-demo/src/main/java/com/jiujianxian/demo/HelloSpringBoot.java
+++ springboot-demo/src/main/java/com/jiujianxian/demo/HelloSpringBoot.java
@@ -1,20 +1,87 @@
 package com.jiujianxian.demo;
 
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+//@Controller
+//public class HelloSpringBoot {
+//	/*@Value("${name}")
+//	private String name;*/
+//	@Autowired
+//	private GirlsProperties girl;
+//	
+//	
+//	@RequestMapping(value="/hello",method=RequestMethod.GET)
+//	public String hello(){
+//		return " Hello spring boot!";
+//	}
+//	@RequestMapping(value="/girl",method=RequestMethod.GET)
+//	public String welcome(){
+//		return girl.getName()+"---"+girl.getAge()+girl.toString();
+//	}
+//	@RequestMapping(value="/welcome",method=RequestMethod.GET)
+//	public String welcome2(){
+//		return "welcome";
+//	}
+//}
+
+
 @RestController
+@RequestMapping(value="/boot")
 public class HelloSpringBoot {
-//	@Value("${name}")
-//	private String name;
+	/*@Value("${name}")
+	private String name;*/
 	@Autowired
 	private GirlsProperties girl;
-	@RequestMapping(value="/hello",method=RequestMethod.GET)
+	
+	@RequestMapping(value="/hello",method=RequestMethod.POST)
 	public String hello(){
-//		return name+" Hello spring boot!";
-		girl.setAge("19");
-		return girl.getName()+"\r\n"+girl.getAge()+"\n"+girl.getCup();
+		return " Hello spring boot!";
+	}
+	@RequestMapping(value="/girl")   //不推荐缺失method参数
+	public String welcome(){
+		return girl.getName()+"---"+girl.getAge()+girl.toString();
+	}
+	@RequestMapping(value={"/welcome","/hi"},method=RequestMethod.GET)
+	public String welcome2(){
+		return "welcome";
+	}
+//	@RequestMapping(value="/cool/{ids}/{name}",method=RequestMethod.GET)
+//	public String cool(@PathVariable("ids") Integer id,@PathVariable("name") String name) {
+//		String cool=id+"\t"+name;
+//		return cool;
+//	}
+	
+	@RequestMapping(value="/cool2",method=RequestMethod.GET)
+	public String cool2(@PathParam("id22") Integer myid,@PathParam("name") String name) {
+		String cool=myid+"\t"+name;
+		return cool;
+	}
+	
+	@RequestMapping(value="/cool3",method=RequestMethod.GET)
+	public String cool3( Integer myid,String name) {
+		String cool=myid+"\t"+name;
+		return cool;
+	}
+	@GetMapping(value="/cool4")
+	public String cool4( Integer myid,String name) {
+		String cool=myid+"\t"+name;
+		return cool;
+	}
+	@PostMapping(value="/cool5")
+	public String cool5( Integer myid,String name) {
+		String cool=myid+"\t"+name;
+		return cool;
 	}
 }
