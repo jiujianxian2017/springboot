c01dd45f5871d9e01879d009abe03750909958a6
zhangwei24863 <zhangwei24863@hundsun.com>
2018-11-09 11:20:36 +0800

    BBEP-Link v0.0.1

---
+++ bbep-links/src/main/java/com/hundsun/link/BbepLinksApplication.java
@@ -0,0 +1,12 @@
+package com.hundsun.link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbepLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbepLinksApplication.class, args);
+	}
+}
